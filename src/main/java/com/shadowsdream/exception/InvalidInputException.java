ae6e73f7d5670dd6024bd128c0dc1e02845ff6b8
ShadowsDream <fedorovpavlo@gmail.com>
2019-11-16 13:26:04 +0200

    [HW-25] First commit

---
+++ src/main/java/com/shadowsdream/exception/InvalidInputException.java
@@ -0,0 +1,11 @@
+package com.shadowsdream.exception;
+
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
