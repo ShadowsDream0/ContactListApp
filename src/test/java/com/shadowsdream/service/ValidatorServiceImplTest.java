feee31ac67bf9288c6ba69dc6cb6ab39035a16cb
ShadowsDream <fedorovpavlo@gmail.com>
2019-11-18 19:17:18 +0200

    [HW-26] First commit

---
+++ src/test/java/com/shadowsdream/service/ValidatorServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.shadowsdream.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorServiceImplTest {
+
+    @org.junit.jupiter.api.Test
+    void validateEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validateAndGetBirthday() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validatePhoneNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void validateProperName() {
+    }
+}
