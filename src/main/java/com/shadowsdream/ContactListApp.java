31aa0ae2f9a97a979760855dd5e3aadb6f4e4465
ShadowsDream <fedorovpavlo@gmail.com>
2022-05-01 11:25:03 +0300

    set up initial spring settings

---
+++ src/main/java/com/shadowsdream/ContactListApp.java
@@ -0,0 +1,12 @@
+package com.shadowsdream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ContactListApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ContactListApp.class, args);
+    }
+}
